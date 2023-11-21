bae3fcc0cf3110fe2e768c8b64680a73a7d321d7
Akash <131151917+Akash2914@users.noreply.github.com>
2023-11-21 23:30:06 +0530

    Update Navbar.jsx
    Resume updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -71,7 +71,7 @@ export const Navbar = () => {
           </li>
           <li className="nav__list-item">
             <a
-              href="https://drive.google.com/drive/folders/1rkbwNClj8JGCi4ehJhOpFAz1bt-RM_US"
+              href="https://drive.google.com/drive/folders/1rkbwNClj8JGCi4ehJhOpFAz1bt-RM_US?usp=sharing"
               onClick={toggleNavList}
               className="link link--nav"
               target="_blank"
